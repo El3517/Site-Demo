e13932b6cbd2392714ec455be83ded30e0b20774
Eneje Caleb <enejecaleb@gmail.com>
2024-12-19 18:48:39 +0100

    popup contact form done

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function hideSideBar(){
     const sideBar = document.querySelector(".sidebar");
     sideBar.style.display = "none";
 }
+
+
+let popup = document.getElementById("contact-popup");
+
+function openPopup(){
+    popup.classList.add("open-contact-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-contact-popup")
+}
